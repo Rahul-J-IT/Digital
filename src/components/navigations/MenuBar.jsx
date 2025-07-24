7da31a4d7b70446bbf3c257a4bb86c84d98c6b4b
Rahul-J-IT <rahul22it203@gmail.com>
2025-07-24 11:36:02 +0530

    changes

--- src/components/navigations/MenuBar.jsx
+++ src/components/navigations/MenuBar.jsx
@@ -152,17 +152,8 @@ const MenuBar = () => {
       >
         <div className="nav-wrapper">
           <a className="logo" href="/">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 2L2 7L12 12L22 7L12 2Z" fill="#4A90E2" />
-              <path d="M2 17L12 22L22 17" stroke="#4A90E2" strokeWidth="2" />
-              <path d="M2 12L12 17L22 12" stroke="#4A90E2" strokeWidth="2" />
-            </svg>
+            <img src="/image.png" alt="" />
+             
             Build works
           </a>
 
